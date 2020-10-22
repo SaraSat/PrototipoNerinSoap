d44052e21c493f40445e5967a86ec4fe4e49d556
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-10-22 15:57:29 +0200

    carousel responsive

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -41,9 +41,34 @@ export class InicioComponent implements OnInit {
 
   ]
 
+  responsiveOptions = [];
 
   constructor() { 
     
+  this.responsiveOptions = [
+    {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+    },
+    {
+      breakpoint: '360px',
+      numVisible: 1,
+      numScroll: 1
+  }
+
+];
+
 
   }
 
